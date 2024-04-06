085882ad964f09c1eed1bd8cd12df0db7c7e47e9
Damir <gaifullindr@gmail.com>
2024-04-07 00:26:43 +0300

    Работа над структурой проекта

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from "@nestjs/common";
+import { Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
-@Controller()
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-}
+    private appService;
+    constructor(appService: AppService) {
+        this.appService = appService;
+    }
+
+    @Get('/users')
+    getUsers() {
+        return this.appService.getUsers();
+    }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+async function start() {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
+};
+
+start();
