639a1fdc6c0bac7ffd5f849b1c437ae7581377db
Damir <gaifullindr@gmail.com>
2024-04-07 01:55:04 +0300

    Реализовал подключение к БД Postgres(Sequalize). Использовал .env (cross-env).

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService]
+    controllers: [],
+    providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: process.env.POSTGRES_HOST,
+          port: Number(process.env.POSTGRES_PORT),
+          username: process.env.POSTGRES_USERNAME,
+          password: process.env.POSTGRES_PASSWORD ,
+          database: process.env.POSTGRES_DATABASE,
+          models: [],
+          autoLoadModels: true
+        }),
+        UsersModule,
+      ]
 })
 export class AppModule {
 
