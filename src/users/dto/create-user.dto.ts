34289345c95dc8c4f3cb5a5c8c1790eaf49a6402
Damir <gaifullindr@gmail.com>
2024-04-09 23:21:00 +0300

    Работа с swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+    @ApiProperty({example: '8364800e-f6ac-11ee-a951-0242ac120002', description: 'uuid.'})
     readonly id: string;
+
+    @ApiProperty({example: 'Name', description: 'Имя пользователя.'})
     readonly name: string;
+
+    @ApiProperty({example: 'Surname', description: 'Фамилия пользователя.'})
     readonly surname: string;
+
+    @ApiProperty({example: 'Patronimic', description: 'Отчество пользователя.'})
     readonly patronimic: string;
+
+    @ApiProperty({example: 'name@gmail.com', description: 'Почта пользователя.'})
     readonly email: string;
+
+    @ApiProperty({example: 'password', description: 'Пароль пользователя.'})
     readonly password: string;
+    
+    @ApiProperty({example: 'student', description: 'Роль пользователя.'})
     readonly role: string;
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 interface UserCreationAttributes {
@@ -14,27 +15,35 @@ interface UserCreationAttributes {
     tableName: 'users'
 })
 export class User extends Model<User, UserCreationAttributes> {
+    @ApiProperty({example: '8364800e-f6ac-11ee-a951-0242ac120002', description: 'uuid.'})
     @Column({type: DataType.UUID, unique: true, primaryKey: true})
     id: string;
 
+    @ApiProperty({example: 'Name', description: 'Имя пользователя.'})
     @Column({type: DataType.STRING, allowNull: false})
     name: string;
 
+    @ApiProperty({example: 'Surname', description: 'Фамилия пользователя.'})
     @Column({type: DataType.STRING, allowNull: false})
     surname: string;
 
+    @ApiProperty({example: 'Patronimic', description: 'Отчество пользователя.'})
     @Column({type: DataType.STRING, allowNull: false})
     patronimic: string;
 
+    @ApiProperty({example: 'image.jpeg', description: 'Фотография пользователя.'})
     @Column({type: DataType.STRING})
     logo: string;
 
+    @ApiProperty({example: 'name@gmail.com', description: 'Почта пользователя.'})
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     email: string;
 
+    @ApiProperty({example: 'password', description: 'Пароль пользователя.'})
     @Column({type: DataType.STRING, allowNull: false})
     password: string;
 
+    @ApiProperty({example: 'student', description: 'Роль пользователя.'})
     @Column({type: DataType.STRING, allowNull: false})
     role: string;
 }
