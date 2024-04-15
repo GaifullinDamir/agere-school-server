0692269dd0e9b69e5f4135866ff8c5bc71cba756
Damir <gaifullindr@gmail.com>
2024-04-15 21:57:28 +0300

    Добавил валидацию.

---
+++ src/pipes/validation.pipe.ts
@@ -0,0 +1,20 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+import { ValidationException } from "src/exceptions/validation.exception";
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+    async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
+        //Преобразовывает значения в нужный для нас класс
+        const obj = plainToClass(metadata.metatype, value);
+        const errors = await validate(obj);
+
+        if (errors.length) {
+            let messages = errors.map(err => {
+                return `${err.property} - ${Object.values(err.constraints).join(', ')}`;
+            })
+            throw new ValidationException(messages);
+        }
+    }
+}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,18 +1,26 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty({example: 'Name', description: 'Имя пользователя.'})
+    @IsString({message: 'Должно быть строкой.'})
     readonly name: string;
 
     @ApiProperty({example: 'Surname', description: 'Фамилия пользователя.'})
-    readonly surname: string;
+    @IsString({message: 'Должно быть строкой.'})
+    readonly surname: string;    
 
     @ApiProperty({example: 'Patronimic', description: 'Отчество пользователя.'})
+    @IsString({message: 'Должно быть строкой.'})
     readonly patronimic: string;
 
     @ApiProperty({example: 'name@gmail.com', description: 'Почта пользователя.'})
+    @IsString({message: 'Должно быть строкой.'})
+    @IsEmail({}, {message: 'Некорректный email.'})
     readonly email: string;
 
     @ApiProperty({example: 'password', description: 'Пароль пользователя.'})
+    @IsString({message: 'Должно быть строкой.'})
+    @Length(8, 16, {message: 'Длине не менее 8 символов и не более 16 символов.'})
     readonly password: string;
 }
