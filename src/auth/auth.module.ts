d5ffc22f3b4be71cd4414b9dc024db038d40b587
Damir <gaifullindr@gmail.com>
2024-04-20 00:10:06 +0300

    Удали set-uuid middleware и ссылки на него.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { SetUuidMiddleware } from 'src/middlewares/set-uuid.middleware';
 
 @Module({
   controllers: [AuthController],
